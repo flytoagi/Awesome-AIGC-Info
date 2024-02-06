b0010e024142903e421961728fb6802d67b7d084
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-06 23:27:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,46 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const assetPrefix = "";
+const basePath = "";
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
   latex: true,
   staticImage: true,
-  output: 'export',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  output: 'export'
 })
 
-module.exports = withNextra({
-  i18n: {
-    locales: ['zh-CN', 'en-US'],
-    defaultLocale: 'zh-CN'
+module.exports = {
+  ...withNextra({
+    i18n: {
+      locales: ['zh-CN', 'en-US'],
+      defaultLocale: 'zh-CN'
+    },
+    // domains: [{
+    //   domain: 'info.ai4agi.org',
+    //   defaultLocale: 'zh-CN'
+    // },
+    // {
+    //   domain: 'info.ai4agi.cn',
+    //   defaultLocale: 'zh-CN'
+    // }
+    // ]
+  }
+  ),
+  images: {
+    unoptimized: true,
   },
-  // domains: [{
-  //   domain: 'info.ai4agi.org',
-  //   defaultLocale: 'zh-CN'
-  // },
-  // {
-  //   domain: 'info.ai4agi.cn',
-  //   defaultLocale: 'zh-CN'
-  // }
-  // ]
 }
-)
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
