4aa31c9978abd7f5a24bf281969c6f78e91b0a3f
RongkangXiong <earth@mail.ustc.edu.cn>
2024-02-06 23:03:10 +0800

    fix: nextjs config export output

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
   latex: true,
-  staticImage: true
+  staticImage: true,
+  output: 'export',
 })
 
 module.exports = withNextra({
